2b899347005d9bec3e62b80745f6927146e1fe77
ChaimB <Chaimb@ravtech.co.il>
2022-09-29 20:47:51 +0300

    .

--- src/pages/GridTamplates/FiveCellGrid/FiveCellGrid.js
+++ src/pages/GridTamplates/FiveCellGrid/FiveCellGrid.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import { FullScreen, useFullScreenHandle } from "react-full-screen";
-import {TextDisplayAnimation} from '../../../components/TextDisplay/TextDisplayAnimation';
+import { TextDisplayAnimation } from '../../../components/TextDisplay/TextDisplayAnimation';
 import ScreenKosherZmanim from '../../../components/HomePage/ScreenKosherZmanim';
 import './FiveCellGrid.scss';
 
 function FiveCellGrid(props) {
   const handle = useFullScreenHandle();
-  let text =` 
+  let text = ` 
   //    <p>&nbsp;</p>
   <p>'הרב הגאון ר <h1><strong>  שמחה בונים&nbsp;</strong></h1>ב"ר <h1><strong>ישראל</strong></h1> זצוקלל"ה</p>
   <p>&nbsp;</p>
@@ -14,7 +14,7 @@ function FiveCellGrid(props) {
   <p>&nbsp;</p>
   <p><strong>התפילות והלימוד לעילוי נשמתו</strong></p>
   <p>&nbsp;</p>`;
-    let text1 =`
+  let text1 = `
     
     <p><נא לכווין בברכות לעי
     נא לכווין בברכות לעילוי נשמתו אבגדהוזחטיכלמנסעפצקשת ולברך בכוונה
@@ -25,24 +25,21 @@ function FiveCellGrid(props) {
     <span> <p>a s d f g h j k l</p>&nbsp;<h1>מה קורה אחי</h1>&nbsp;<p>a s d f g h j k l</p>&nbsp;<h1>מה קורה אחי</h1>&nbsp;<p>a s d f g h j k l</p>&nbsp;<h1>מה קורה אחי</h1>&nbsp;<p>a s d f g h j k l</p>&nbsp;<h1>מה קורה אחי</h1>&nbsp;<p>a s d f g h j k l</p>&nbsp;<h1>מה קורה אחי</h1>&nbsp;<p>a s d f g h j k l</p>&nbsp;<h1>מה קורה אחי</h1>&nbsp;<p>a s d f g h j k l</p>&nbsp;<h1>מה קורה אחי</h1> &nbsp;<p>a s d f g h j k l</p></span>
     <h1>מה קורה אחי</h1>
      `;
-     let text3 = `<span> <p>a s d f g h j k l</p>&nbsp;<h1>מה קורה אחי</h1>&nbsp;<p>a s d f g h j k l</p>&nbsp;<h1>מה קורה אחי</h1>&nbsp;<p>a s d f g h j k l</p>&nbsp;<h1>מה קורה אחי</h1>&nbsp;<p>a s d f g h j k l</p>&nbsp;<h1>מה קורה אחי</h1>&nbsp;<p>a s d f g h j k l</p>&nbsp;<h1>מה קורה אחי</h1>&nbsp;<p>a s d f g h j k l</p>&nbsp;<h1>מה קורה אחי</h1>&nbsp;<p>a s d f g h j k l</p>&nbsp;<h1>מה קורה אחי</h1> &nbsp;<p>a s d f g h j k l</p></span> `
+  let text3 = `<span> <p>a s d f g h j k l</p>&nbsp;<h1>מה קורה אחי</h1>&nbsp;<p>a s d f g h j k l</p>&nbsp;<h1>מה קורה אחי</h1>&nbsp;<p>a s d f g h j k l</p>&nbsp;<h1>מה קורה אחי</h1>&nbsp;<p>a s d f g h j k l</p>&nbsp;<h1>מה קורה אחי</h1>&nbsp;<p>a s d f g h j k l</p>&nbsp;<h1>מה קורה אחי</h1>&nbsp;<p>a s d f g h j k l</p>&nbsp;<h1>מה קורה אחי</h1>&nbsp;<p>a s d f g h j k l</p>&nbsp;<h1>מה קורה אחי</h1> &nbsp;<p>a s d f g h j k l</p></span> `
   return (
     <section>
       <button onClick={handle.enter}>
-         מסך מלא
-    </button>
+        מסך מלא
+      </button>
 
       <FullScreen handle={handle}>
         <div className="five-grid-container">
-          <div className="main">
-            <ScreenKosherZmanim />
-            </div>
-          <div className="header">
-            </div>
-          <div className="footer"><TextDisplayAnimation direction ="horizontal" id={Math.random() * 100} text={text3}/></div>
-          <div className="right-top"><TextDisplayAnimation direction ="vertical" id={Math.random() * 100} text={text}/></div>
+          <div className="main"><ScreenKosherZmanim /></div>
+          <div className="header"></div>
+          <div className="footer"><TextDisplayAnimation direction="horizontal" id={Math.random() * 100} text={text3} /></div>
+          <div className="right-top"><TextDisplayAnimation direction="vertical" id={Math.random() * 100} text={text} /></div>
           <div className="right-bottom"></div>
-          <div className="left-top"><TextDisplayAnimation direction ="vertical" id={Math.random() * 100} text={text1}/></div> 
+          <div className="left-top"><TextDisplayAnimation direction="vertical" id={Math.random() * 100} text={text1} /></div>
           <div className="left-bottom"></div>
         </div>
       </FullScreen>
@@ -60,7 +57,7 @@ export default FiveCellGrid;
 // import {TextDisplayAnimation} from '../../components/TextDisplay/TextDisplayAnimation';
 
 // function ExmpleForAnimationComponents() {
-//  
+//
 //   return (
 //     <div className='ExmpleForAnimationComponents'>
 //         <div className='item1'>
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,87 @@
-/** @type {import('tailwindcss').Config} */
+/* eslint-disable global-require */
 module.exports = {
-  content: [],
-  theme: {
-    extend: {},
+  mode: 'jit',
+  future: {
+    removeDeprecatedGapUtilities: true,
+    purgeLayersByDefault: true,
   },
-  plugins: [],
-}
+  purge: ['./src//components/**/*.js', './src/pages/**/*.js'],
+  variants: {
+    extend: {
+      backgroundColor: ['active', 'checked'],
+      maxHeight: ['focus'],
+      borderColor: ['checked'],
+    },
+  },
+  plugins: [
+    require('tailwindcss'),
+    require('precss'),
+    require('autoprefixer'),
+    require('tailwind-scrollbar'),
+    require('@tailwindcss/typography'),
+  ],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {
+      colors: {
+        orange: {
+          DEFAULT: '#F19672',
+          active: '#D0886D',
+          mainOr: '#FB9773',
+        },
+        white: {
+          DEFAULT: '#FFFFFF',
+          active: ' #F3F3F3',
+        },
+        black: '#000000',
+        red: {
+          DEFAULT: '#F97168',
+          error: '#FF8282',
+          active: '#FF8282',
+        },
+        green: {
+          DEFAULT: '#FF3344',
+          light: '#B3DDCF',
+          dark: '#15858E',
+          lighter: '#A8DFCE',
+          500: '#41C2C4',
+          success: '#DBFFE6',
+          successBorder: '#7AD77F',
+        },
+        blue: {
+          DEFAULT: '#3C91A0',
+          light: '#45A4C7',
+          dark: '#252464',
+          active: '#429FAB',
+        },
+        gray: {
+          DEFAULT: '#6C6C6C',
+          light: '#CCCCCC',
+          dark: '#4C4C4C',
+          lighter: '#EEEEEE',
+          disabled: '#E7E7E7',
+          text: '#B7B7B7',
+          active: '#999999',
+          mid: '#333333',
+        },
+        yellow: '#FFC960',
+        gradient: {
+          1: '#40C2C4',
+          2: '#3C91A0',
+        },
+      },
+      // spacing: {
+      //   registerPageInputHeight: '50px',
+      // },
+      backgroundColor: (theme) => ({
+        ...theme('colors'),
+        mainOrange: '#FB9773',
+        lightgreybackground: '#F5F5F5',
+        registercontainer_white: '#FFFFFF',
+        registercontainer_Input_grey: '#FFFFFF',
+        registerPageInputGrey: '#CCCCCC',
+        registerPageButtonGrey: '#999999',
+      }),
+    },
+  },
+};
