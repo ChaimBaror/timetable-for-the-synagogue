809b306cf42fb98984dc27f09f10aa2c58fea5d4
chaim baror <chbaror@gmail.com>
2022-11-09 22:38:01 +0200

    add ;oop

--- src/pages/FiveCellGrid.tsx
+++ src/pages/FiveCellGrid.tsx
@@ -8,23 +8,39 @@ import HebcalZmanim from '../components/HebcalZmanim'
 import Clock from '../components/Clock'
 
 const FiveCellGrid: React.FC = () => {
+    const footertext = 'הלימוד והתפילות לע"נ יצחק מנדל בן ר אהרון * הברכות להצלחת חיים מרדכי בן ר יוסף  * הלימוד לרפואת אילן ישי בן שושנה'
     return (
-        <div className='continer'>
-            <div className='list_of_zmanim'>
-                <h1 className='date_information'>זמני תפילה</h1>
-                {list_zmanim_Tfila.map(({ name, zmanim }) => (
-                    <ItemList key={name} name={`${name}:`} zmanim={zmanim} />)
-                )}
-            </div>
-            <div>
-                <HebcalZmanim />
-                <div className="ClockAnalog">
-                    <Clock />
-                    <ClockAnalog />
+        <>
+
+            <div className='continer'>
+                <div className='list_of_zmanim'>
+                    <h1 className='date_information'>זמני תפילה</h1>
+                    {list_zmanim_Tfila.map(({ name, zmanim }) => (
+                        <ItemList key={name} name={`${name}:`} zmanim={zmanim} />)
+                    )}
+                </div>
+                <div>
+                    <HebcalZmanim />
+                    <div className="ClockAnalog">
+                        <Clock />
+                        <ClockAnalog />
+                    </div>
+                </div>
+                <div><ScreenKosherZmanim /></div>
+            </div >
+            <div className='footer'>
+                <div className="marquee-section">
+                    <div className="marquee-div">
+                        <div className="marquee loop">
+                            &bull; {footertext}   &bull;  {footertext} &bull;  {footertext} &bull;</div>
+                    </div>
                 </div>
             </div>
-            <div><ScreenKosherZmanim /></div>
-        </div >
+                <div className="loop ">
+                    <div className="bg-marquee"></div>
+                </div>
+        </>
+
     )
 }
 
