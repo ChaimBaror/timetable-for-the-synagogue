84a911a023d12bb06517ef34dbed1b681209c2a7
ChaimB <Chaimb@ravtech.co.il>
2022-11-09 13:32:29 +0200

    BrowserRouter

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,13 @@
 import React from 'react';
 import { AppRouter } from './router/AppRouter';
 import { ProSidebarProvider } from 'react-pro-sidebar';
-import { BrowserRouter } from 'react-router-dom'
 
 function App() {
 
   return (
     <>
       {/* <ProSidebarProvider> */}
-      <BrowserRouter basename={process.env.PUBLIC_URL}>
         <AppRouter />
-      </BrowserRouter>
       {/* </ProSidebarProvider>; */}
     </>
   );
