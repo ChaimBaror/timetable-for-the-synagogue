fd9162f9d68f5195a55056fabbc02d316b65486a
Chaim Bar-or <chaimb@ravtech.local>
2023-02-08 15:15:01 +0200

    add hoock

---
+++ src/services/profile.service.ts
@@ -0,0 +1,12 @@
+import { useState } from "react";
+
+export const ProfileAPI = {
+
+    profile: async (query = '') => {
+
+    },
+    //User Profile Details
+    location: async ( query = '') => {
+
+    },
+}
